a267ded5c735ec9b51fb206921c0853078a70e18
sparc373 <srhari.prasad@yahoo.com>
2023-06-30 16:06:29 +0530

    Update EC2-2-regions.tf
    comments updated

--- EC2-2-regions.tf
+++ EC2-2-regions.tf
@@ -14,20 +14,20 @@ provider "aws" {
 
 
 resource "aws_instance" "myserver-o" {
-  ami           = "ami-02f97949d306b597a"
+  ami           = "ami-02f97949d306b597a"   ### Replace AMI as per Intance w.r.t to Region
   instance_type = "t2.micro"
   provider      = aws.east2
   tags = {
-    Name = "hello-ohio"
+    Name = "hello-ohio"   ## Replace Name Tag as required
   }
 }
 
 resource "aws_instance" "myserver-n" {
-  ami           = "ami-00c39f71452c08778"
+  ami           = "ami-00c39f71452c08778"   ### Replace AMI as per Intance w.r.t to Region
   instance_type = "t2.micro"
   provider      = aws.east1
   tags = {
-    Name = "hello-virginia"
+    Name = "hello-virginia" ## Replace Name Tag as required
   }
 }
 
